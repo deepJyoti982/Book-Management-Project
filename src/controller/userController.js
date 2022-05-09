5b1b37363ac0e26f9d75e4d481c853807df659d6
unknown <deepsarkar982@gmail.com>
2022-05-09 17:32:58 +0530

    user login

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -1,4 +1,6 @@
 const UserModel = require("../modules/UserModel");
+const jsonwebtoken = require('jsonwebtoken');
+const { functions } = require("lodash");
 
 
 
@@ -18,4 +20,34 @@ const registerUser = async (req, res) => {
     }
 }
 
-module.exports = { registerUser };
+
+const loginUser = async function(req,res) {
+    try {
+        const loginData = req.body;
+        //validation needed
+        const {email,password} = loginData
+        //validation needed
+        const user = await UserModel.findOne({email: email, password: password})
+        if(!user) {
+            return res.status( 400 ).send({ status: false, msg: "username or the password is not correct" })
+        }
+
+        let token = jwt.sign(
+            {
+                userId: user._id.toString(),
+                batch: "Uranium",
+                organisation: "FunctionUp",
+            },
+            "functionUp-Uranium"
+        )
+        res.setHeader("x-api-key",token)
+        res.send({status: true, data: token})
+    }
+    catch( error ){
+        res.status( 500 ).send(error.message)
+    }
+}
+module.exports = { 
+    registerUser,
+    loginUser 
+};
